816a7cf2b95113e7ce4af7938eb4b7120c14d440
handsome0001 <45452717+handsome0001@users.noreply.github.com>
2018-11-29 20:12:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -33,13 +33,15 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text= Reply(event.message.text))
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 def Reply(text):
 	if text　== "sb":
-	    return 'sds'
-    
+		return 'sds'
+        
+
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
