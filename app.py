a19532af2bc1f5196ae007f1948b15fa86b6d9f4
handsome0001 <45452717+handsome0001@users.noreply.github.com>
2018-12-20 18:59:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -95,10 +95,10 @@ def Reply(event):
 def handle_message(event):
     try:
         Reply(event)
-        '''
-        line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.source.user_id + "說:"))
-        line_bot_api.push_message("U95418ebc4fffefdd89088d6f9dabd75b", TextSendMessage(text=event.message.text))
-        '''
+        
+        line_bot_api.push_message("U7dcdd11e5e85c7b55edcff3398533cd4", TextSendMessage(text=event.source.user_id + "說:"))
+        line_bot_api.push_message("U7dcdd11e5e85c7b55edcff3398533cd4", TextSendMessage(text=event.message.text))
+        
     except Exception as e:
         line_bot_api.reply_message(event.reply_token, 
             TextSendMessage(text=str(e)))
